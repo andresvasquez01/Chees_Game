0fc93f8591da102661b8b5b5049d03e8e550619b
andresvasquez01 <142435709+andresvasquez01@users.noreply.github.com>
2024-02-12 15:42:05 -0500

    Chess Game With MVC Applied

---
+++ ChessMVC/src/View/Main.java
@@ -0,0 +1,25 @@
+package View;
+
+import javax.swing.JFrame;
+
+public class Main {
+	
+	public static void main(String[]args) {
+		
+		JFrame window = new JFrame("Chess");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		
+		// add GamePanel to the window
+		GamePanel gp = new GamePanel();
+		window.add(gp);
+		window.pack();
+		
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		
+		gp.launchGame();
+		
+	} 
+
+}
